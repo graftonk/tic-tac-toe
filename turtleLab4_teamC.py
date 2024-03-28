0966ad55565203dd7d40213a586a6486257c6f6a
yasminabedin3 <165226953+yasminabedin3@users.noreply.github.com>
2024-03-27 21:18:58 -0400

    fix: docstring after the def line
    see guide here: https://peps.python.org/pep-0008/#documentation-strings

--- turtleLab4_teamC.py
+++ turtleLab4_teamC.py
@@ -1,5 +1,4 @@
 '''
-
 Program Name:      turtleLab4_teamC.py
 Authors:           Kaylee Grafton, Frederika Delcuore, Samuel Larocca, Fatema Yasmin, Brendan Loukes
 Description:       This program is a game of Tic-Tac-Toe between two players using the turtle library.
@@ -17,12 +16,12 @@ Checklist:         Main function
 
 import turtle
 
-'''
-Function drawBoard
-Written by:            Kaylee Grafton
-Description:           Sets up the turtle screen and draws the game board using turtle
-'''
 def drawBoard(board_size, cell_size):
+    '''
+    Function drawBoard
+    Written by:            Kaylee Grafton
+    Description:           Sets up the turtle screen and draws the game board using turtle
+    '''
     screen_size = board_size * cell_size
     padding = 75 #extra space outside of the grid for the screen
 
@@ -57,12 +56,12 @@ def drawBoard(board_size, cell_size):
     screen.onscreenclick(lambda x, y: mouseClick(x, y, board_size, cell_size)) #gets coords of mouse click
     screen.mainloop() #keeps the window open
 
-'''
-Function mouseClick
-Written by:             Kaylee Grafton
-Description:            When the player clicks on the screen, returns the row and column of the cell.
-'''
 def mouseClick(x, y, board_size, cell_size):
+    '''
+    Function mouseClick
+    Written by:             Kaylee Grafton
+    Description:            When the player clicks on the screen, returns the row and column of the cell.
+    '''
     grid_offset = (cell_size * board_size) / 2 #offsets the negative number on the grid
 
     #determines row and col based on mouse coords, starts at 0
@@ -71,18 +70,18 @@ def mouseClick(x, y, board_size, cell_size):
 
     return row, col
 
-'''
-Function initializeBoard
-Written by:             Fatema Yasmin
-Description:            initialize the game board as a 2d list
-'''
 def initializeBoard(board_size):
+    '''
+    Function initializeBoard
+    Written by:             Fatema Yasmin
+    Description:            initialize the game board as a 2d list
+    '''
     return [[' ' for _ in range(board_size)] for _ in range(board_size)]
 
-'''
-Function main
-'''
 def main():
+    '''
+    Function main
+    '''
     board_size = 3 #how many cells each row/col gets
     cell_size = 100 #can change this value to make the board and screen bigger or smaller
     board = initializeBoard(board_size) # initialize the game board
